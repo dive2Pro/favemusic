330fcedb0a85592c99609c8c6b895be53e5213e1
hyc <toylucklebo@gmail.com>
2017-01-02 14:17:02 +0800

    show track actions deffrently

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -3,39 +3,45 @@ import { getTrackIcon } from '../utils/track'
 import { isSameTrackAndPlaying } from '../utils/player.js'
 export default class Track extends Component {
 
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.renderImage = this.renderImage.bind(this)
   }
 
-  renderImage(artwork_url, title, avatar_url) {
+  renderImage (artwork_url, title, avatar_url) {
     return (
       <div>
         <img src={artwork_url || avatar_url} alt={title} height='90' width='90'/>
       </div>
     )
   }
-  renderOverlay(activity, activateTrack, isPlaying) {
-    const {activeTrack, addTrackToPlaylist} = this.props
-    const {origin} = activity
-    const {stream_url} = origin
+
+  renderActions (activity, activateTrack, isPlaying) {
+    const { activeTrack, addTrackToPlaylist } = this.props
+    const { origin } = activity
+    const { stream_url } = origin
     if (!stream_url) return
 
     const currentTrackIsPlaying = isSameTrackAndPlaying(activeTrack, activity, isPlaying)
 
     return (
-      <div className="track-img-overlay">
-        <i className={`fa ${currentTrackIsPlaying ? 'fa-pause' : 'fa-play'}`}
-          onClick={() => activateTrack(activity)}  > </i> 
+      <div className="track-actions">
+        <div className="track-actions-item">
+          <i className={`fa ${currentTrackIsPlaying ? 'fa-pause' : 'fa-play'}`}
+             onClick={() => activateTrack(activity)}> </i>
+        </div>
 
-        <i className="fa fa-list"
-          onClick={() => addTrackToPlaylist(activity)}  > </i>
+        <div className="track-actions-item">
+          <i className="fa fa-list"
+             onClick={() => addTrackToPlaylist(activity)}> </i>
+        </div>
       </div>
     )
   }
-  render() {
 
-    const { activity, activateTrack, isPlaying} = this.props
+  render () {
+
+    const { activity, activateTrack, isPlaying } = this.props
     const { origin, type } = activity
     if (!origin) return (<div></div>)
     const { user, title, permalink_url, artwork_url } = origin
@@ -44,17 +50,14 @@ export default class Track extends Component {
     return (
       <div className="track">
         <div className="track-img"
-         >
-
+        >
           {this.renderImage(artwork_url, title, avatar_url)}
-
-          {this.renderOverlay(activity, activateTrack, isPlaying)}
-
         </div>
         <div className="track-content">
           <a href={permalink_url}>
             <i className={getTrackIcon(type)}> </i> &nbsp; {username} - {title}</a>
         </div>
+        {this.renderActions(activity, activateTrack, isPlaying)}
       </div>
     )
   }
