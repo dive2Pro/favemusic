33db85f6db89b58abb94563694b40e62396c005c
hyc <toylucklebo@gmail.com>
2017-01-11 21:49:56 +0800

    test(Action):spec

--- src/components/Artwork.spec.js
+++ src/components/Artwork.spec.js
@@ -1,70 +1,39 @@
-import Actions, { Action } from './Actions'
+import Artwork from './Artwork'
 
-describe('Actions Test', () => {
-  it('render, show action  ', done => {
-    const configuration = [
-      { x: 'x1' }, { y: 'y1' }
-    ]
-    const isVisible = false
+describe('Artwork Test', () => {
+  it('render, show img with src equal image  ', done => {
     const props = {
-      configuration
-      , isVisible
+      alt: 'hyc'
+      , image: 'www.baidu'
+      , optionalImg: 'www.google'
+      , size: 19
     }
 
-    const element = shallow(<Actions {...props} />)
-    expect(element.find('.action')).to.have.length(1)
-    expect(element.find('.action-visible')).to.have.length(0)
-    done()
-  }
-  )
-  it('render, show action-visible  ', done => {
-    const configuration = [
-      { x: 'x1' }, { y: 'y1' }
-    ]
-    const isVisible = true
-    const props = {
-      configuration
-      , isVisible
-    }
-
-    const element = shallow(<Actions {...props} />)
-    expect(element.find('.action')).to.have.length(1)
-    expect(element.find('.action-visible')).to.have.length(1)
-    done()
-  }
-  )
-  it('render, show Action length eq   ', done => {
-    const configuration = [
-      { x: 'x1' }, { y: 'y1' }
-    ]
-    const isVisible = true
-    const props = {
-      configuration
-      , isVisible
-    }
-
-    const element = shallow(<Actions {...props} />)
-    expect(element.find('.action')).to.have.length(1)
-    expect(element.find('.action-visible')).to.have.length(1)
-    expect(element.find('Action')).to.have.length(configuration.length)
+    const element = shallow(<Artwork {...props} />)
+    expect(element.find('img')).to.have.length(1)
+    expect(element.find('img').prop('alt')).to.eq(props.alt)
+    expect(element.find('img').prop('src')).to.eq(props.image)
+    expect(element.find('img').prop('height')).to.eq(props.size)
+    expect(element.find('img').prop('width')).to.eq(props.size)
 
     done()
   }
   )
-}
-)
-describe('Action Test', () => {
-  it('render, Action   ', done => {
+  it('render, show img with src equal optionalImg  ', done => {
     const props = {
-      cfg: {
-        fn: () => { }
-        , className: 'hyc'
-      }
+      alt: 'hyc'
+      , image: null
+      , optionalImg: 'www.google'
+      , size: 19
     }
 
-    const element = shallow(<Action {...props} />)
-    expect(element.find('.actions-item')).to.have.length(1)
-    expect(element.find('i').prop('class')).to.eq(props.className)
+    const element = shallow(<Artwork {...props} />)
+    expect(element.find('img')).to.have.length(1)
+    expect(element.find('img').prop('alt')).to.eq(props.alt)
+    expect(element.find('img').prop('src')).to.eq(props.optionalImg)
+    expect(element.find('img').prop('height')).to.eq(props.size)
+    expect(element.find('img').prop('width')).to.eq(props.size)
+
     done()
   }
   )
